ce9e6d67af739f0930d943b73a19156b407b7da1
Daniel Scott <danielscott886@gmail.com>
2021-08-30 14:30:35 +0100

    added source fonts

--- gen.sh
+++ gen.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # Generate fbpad fonts
 
-FP="/path/to/fonts"
+FP="./fonts"
 OP="-h23 -w17"
 SZ="12h85v85r-3"
-./mkfn_ft $OP $FP/UbuntuMono-R.ttf:$SZ	>reg.tf
-./mkfn_ft $OP $FP/UbuntuMono-RI.ttf:$SZ	>ital.tf
-./mkfn_ft $OP $FP/UbuntuMono-B.ttf:$SZ	>bold.tf
+./mkfn_ft $OP $FP/Hack\ Regular\ Nerd\ Font\ Complete\ Mono.ttf:$SZ	>reg.tf
+./mkfn_ft $OP $FP/Hack\ Italic\ Nerd\ Font\ Complete\ Mono.ttf:$SZ	>ital.tf
+./mkfn_ft $OP $FP/Hack\ Bold\ Nerd\ Font\ Complete\ Mono.ttf:$SZ	>bold.tf
